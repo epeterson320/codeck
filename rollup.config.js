4182e1cf7fbe7cc851af937df77e4bbd604c390d
Eric Peterson <epeterson@sanfordguide.com>
2017-03-10 11:15:03 -0500

    Add working UI plus card stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,21 @@
 import cjsToES6 from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'src/app.js',
-  format: 'iife',
-  plugins: [ cjsToES6(), resolve() ],
-  external: ['big.js'],
-  exports: 'none',
   dest: 'static/bundle.js',
+  format: 'iife',
+  exports: 'none',
+  globals: {
+    'big.js': 'Big',
+    'd3': 'd3',
+  },
+  plugins: [
+    cjsToES6(), // libraries go from commonjs to es6
+    resolve(), // our source code and transpiled libraries use es6
+    babel({ // generate es5 for browser compatibility
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+  ],
 };
--- src/app.js
+++ src/app.js
@@ -1,12 +1,45 @@
+import * as d3 from 'd3';
 import Codeck from './Codeck';
 
-const messageEl = document.getElementById('message');
-const cardsEl = document.getElementById('cards');
-
+const { floor } = Math;
 const codeck = new Codeck();
 
-messageEl.onkeyup = function(e) {
-  const message = e.target.value.toLowerCase();
-  const cards = codeck.encode(message);
-  cardsEl.innerHTML = cards.join(', ');
-};
+const svg = d3.select('#cards')
+  .append('svg')
+  .attr('width', '600px')
+  .attr('height', '400px');
+
+let cards = codeck.deck;
+let message = '';
+let timeoutID = -1;
+
+d3.select('#message').on('keyup', function() {
+  clearTimeout(timeoutID);
+  timeoutID = setTimeout(updateCards, 300);
+});
+
+function updateCards() {
+  message = d3.select('#message').node().value.toLowerCase().trim();
+  cards = codeck.encode(message);
+
+  const cardSel = svg.selectAll('.card').data(cards, c => c);
+
+  const newcards = cardSel.enter().append('g').attr('class', 'card');
+
+  /*newcards.append('rect')
+    .attr('width', 36)
+    .attr('height', 36);*/
+
+  newcards.append('text')
+    .text((d) => d)
+    //.attr('x', 8)
+    .attr('y', 60)
+    .attr('fill', (d, i) => (i > 26) ? 'red' : 'black');
+
+  cardSel.transition()
+    .delay((d, i) => i * 5)
+    .attr('transform', (d) => {
+      const i = cards.indexOf(d);
+      return `translate(${(i % 9) * 48}, ${60 * floor(i / 9)})`;
+    });
+}
