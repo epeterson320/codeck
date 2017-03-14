3aa77110f7885d667450ac9b698dd40ff362d299
Eric Peterson <epeterson@sanfordguide.com>
2017-03-14 15:41:50 -0400

    Remove network waterfall

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,20 @@
 import cjsToES6 from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/app.js',
   dest: 'dist/bundle.js',
   format: 'iife',
   exports: 'none',
+  sourceMap: true,
   plugins: [
     cjsToES6(), // transpile commonjs libraries to es6
     resolve(), // load es6 modules correctly
     babel({ // generate es5 for browser compatibility
       exclude: 'node_modules/**' // only transpile our source code
     }),
+    uglify(),
   ],
 };
