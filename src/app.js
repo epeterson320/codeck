2c13c609d6e06e5954de1a6b5c2c42332fed4d98
Eric <epeterson320@gmail.com>
2017-05-15 23:20:47 -0400

    Fix cards jumping bug

--- src/app.js
+++ src/app.js
@@ -10,9 +10,12 @@ import Codeck from './Codeck'
 const { floor } = Math
 const codeck = new Codeck()
 
+const width = Math.min(512, window.innerWidth - 32)
+const height = 400
+
 const svg = select('#cards svg')
-  .attr('width', '512px')
-  .attr('height', '400px')
+  .attr('width', width)
+  .attr('height', height)
 
 const messageInput = select('#message')
 const cleanedMessageSpan = select('.cleanednotification')
@@ -59,7 +62,9 @@ function updateMessage () {
 }
 
 function moveCard (card, di) {
-  if (card + di > cardOrder.length || card + di < 0) return
+  if (card.idx + di >= cardOrder.length || card.idx + di < 0) return false
+  if (card.idx % 9 === 8 && di === 1) return false
+  if (card.idx % 9 === 0 && di === -1) return false
 
   if (di > 0) {
     for (let i = card.idx; i < card.idx + di; i++) {
@@ -75,6 +80,7 @@ function moveCard (card, di) {
   cardOrder[card.idx + di] = card.key
   card.idx += di
   renderCards()
+  return true
 }
 
 function renderCards () {
@@ -82,7 +88,7 @@ function renderCards () {
     .delay(d => d.idx * 5)
     .filter(d => !d.dragging)
     .attr('transform', d =>
-      `translate(${d.idx % 9 * 48 + d.dx}, ${floor(d.idx / 9) * 60 + d.dy})`)
+      `translate(${d.idx % 9 * 48 + d.dx}, ${floor(d.idx / 9) * 60 + d.dy + 4})`)
 }
 
 function dragstarted (d) {
@@ -98,17 +104,21 @@ function dragged (d) {
   d.dy += currentEvent.dy
   if (d.dx > 28) {
     d.dx -= 48
-    moveCard(d, 1)
+    const moved = moveCard(d, 1)
+    if (!moved) d.dx += 48
   } else if (d.dx < -28) {
     d.dx += 48
-    moveCard(d, -1)
+    const moved = moveCard(d, -1)
+    if (!moved) d.dx -= 48
   }
   if (d.dy > 34) {
     d.dy -= 60
-    moveCard(d, 9)
+    const moved = moveCard(d, 9)
+    if (!moved) d.dy += 60
   } else if (d.dy < -34) {
     d.dy += 60
-    moveCard(d, -9)
+    const moved = moveCard(d, -9)
+    if (!moved) d.dy -= 60
   }
   select(this).attr('transform', d =>
     `translate(${d.idx % 9 * 48 + d.dx}, ${floor(d.idx / 9) * 60 + d.dy})`)
@@ -135,6 +145,8 @@ function showCardsInit () {
         const g = select(this)
         g.append('rect')
           .attr('fill', 'white')
+          .attr('x', '3px')
+          .attr('y', '1px')
           .style('width', 36)
           .style('height', 46)
         g.append('text')
